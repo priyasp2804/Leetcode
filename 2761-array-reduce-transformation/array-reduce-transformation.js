c7704a56b60984fb1f66455da117debf268507e3
PRIYADARSHINI S P <priyadarshinipadmanaban5@gmail.com>
2025-05-10 23:51:04 +0530

    Time: 46 ms (43.74%) | Memory: 55.3 MB (43.82%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -1,12 +1,9 @@
-var reduce = function(nums, fn, init) {
-    if(nums.length === 0)
-        return init
-    else{
-        let val = init;
-        for(let i=0;i<nums.length;i++)
-        {
-            val = fn(val,nums[i])
-        }
-        return val
+var reduce = function (nums, fn, init) {
+
+    let val = init;
+    for (let i = 0; i < nums.length; i++) {
+        val = fn(val, nums[i])
     }
+    return val
+
 };
