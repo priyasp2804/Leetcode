b37b80485b4af68b72755a668540a99535500a54
PRIYADARSHINI S P <priyadarshinipadmanaban5@gmail.com>
2025-06-27 14:54:43 +0530

    Time: 55 ms (7.34%) | Memory: 53.8 MB (31.63%) - LeetSync

--- 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
@@ -1,6 +1,6 @@
 var argumentsLength = function(...args) {
-    // let count = 0;
-    // for(let n of args)
-    //     count++;
-    return args.length;
-};
+    let count = 0;
+    for(let n of args)
+        count++;
+    return count;
+}
