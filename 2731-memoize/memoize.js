b3ae24b05b3f5eb75cccaf36edd5f9fa4fec5ed1
PRIYADARSHINI S P <priyadarshinipadmanaban5@gmail.com>
2025-06-27 15:49:06 +0530

    Time: 241 ms (93.49%) | Memory: 95.3 MB (25.76%) - LeetSync

--- 2731-memoize/memoize.js
+++ 2731-memoize/memoize.js
@@ -10,15 +10,3 @@ function memoize(fn) {
         return cache.get(key);
     }
 }
-
-
-/** 
- * let callCount = 0;
- * const memoizedFn = memoize(function (a, b) {
- *	 callCount += 1;
- *   return a + b;
- * })
- * memoizedFn(2, 3) // 5
- * memoizedFn(2, 3) // 5
- * console.log(callCount) // 1 
- */
