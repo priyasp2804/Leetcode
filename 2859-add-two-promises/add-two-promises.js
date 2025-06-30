fbf645e481a6deda927bd344ba8cfd29327d3623
PRIYADARSHINI S P <priyadarshinipadmanaban5@gmail.com>
2025-06-30 15:04:38 +0530

    Time: 49 ms (71.38%) | Memory: 54.2 MB (34.71%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -1,11 +1,4 @@
 var addTwoPromises = async function(promise1, promise2) {
-    try{
-        const a = await promise1;
-        const b = await promise2;
-        return(a+b);
-    }
-    catch(error){
-        console.log(error);
-    }
-
+        const [a,b] = await Promise.all([promise1, promise2]);
+        return a+b;
 };
